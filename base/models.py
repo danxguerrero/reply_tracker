d2a025a4b8a5df65f80541642c3514c1af2c8707
danxguerrero <danxguerrero@yahpp.com>
2023-06-17 00:25:08 -0400

    Updated max length of note field to 40 chars

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     ticket = models.CharField(max_length=20)
-    note = models.CharField(max_length=20, null=True, blank=True)
+    note = models.CharField(max_length=40, null=True, blank=True)
     created = models.DateField(auto_now_add=True)
 
     def __str__(self):
